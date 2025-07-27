8665e096719bc3322fc0a2b35c967b93f20e9a70
Toufiq A. Shishir <toufiq_a24@outlook.com>
2025-07-27 17:43:31 +0600

    chore: modified some stuff

--- lua/twfksh/plugins/telescope.lua
+++ lua/twfksh/plugins/telescope.lua
@@ -26,19 +26,24 @@ return { -- Fuzzy Finder (files, lsp, etc)
         layout_strategy = 'horizontal',
         layout_config = {
           horizontal = {
-            -- prompt_position = 'top',
+            prompt_position = 'top',
             preview_width = 0.55,
             results_width = 0.8,
+            mirror = false,
+          },
+          vertical = {
+            mirror = false,
           },
           width = 0.87,
-          height = 0.87,
+          height = 0.85,
           preview_cutoff = 120,
         },
+        sorting_strategy = 'ascending',
         -- Appearance
         -- borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
         use_less = true,
         path_display = { 'truncate' },
-        -- winblend = 10, -- Transparency
+        winblend = 10, -- Transparency
         -- File ignore patterns
         file_ignore_patterns = {
           'node_modules',
