78faeba6e81b55e98c873dc7af8592ac2d1b44ae
Toufiq A. Shishir <toufiq_a24@outlook.com>
2025-07-27 17:21:25 +0600

    chore: modified some stuff

--- lua/twfksh/plugins/mini.lua
+++ lua/twfksh/plugins/mini.lua
@@ -16,16 +16,10 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
 
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
+    -- Statusline config
     local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
     statusline.setup { use_icons = vim.g.have_nerd_font }
 
-    -- You can configure sections in the statusline by overriding their
-    -- default behavior. For example, here we set the section for
-    -- cursor location to LINE:COLUMN
     ---@diagnostic disable-next-line: duplicate-set-field
     statusline.section_location = function()
       return '%2l:%-2v'
@@ -42,5 +36,6 @@ return { -- Collection of various small independent plugins/modules
     end, { desc = 'Open mini files' })
 
     require('mini.indentscope').setup() -- Indent line
+    require 'mini.base16' -- base16 colors
   end,
 }
