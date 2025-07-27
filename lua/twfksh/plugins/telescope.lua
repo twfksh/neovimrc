94594ed83c6924c923018f52c5ad062a5e84d34a
Toufiq A. Shishir <toufiq_a24@outlook.com>
2025-07-27 17:37:03 +0600

    chore: modified some stuff

--- lua/twfksh/plugins/telescope.lua
+++ lua/twfksh/plugins/telescope.lua
@@ -26,12 +26,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
         layout_strategy = 'horizontal',
         layout_config = {
           horizontal = {
-            prompt_position = 'top',
+            -- prompt_position = 'top',
             preview_width = 0.55,
             results_width = 0.8,
-          },
-          vertical = {
-            mirror = false,
           },
           width = 0.87,
           height = 0.87,
