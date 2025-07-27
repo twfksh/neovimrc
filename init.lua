971bdf65873349a64a52d16fc6a4fb28694b6bec
Toufiq A. Shishir <toufiq_a24@outlook.com>
2025-07-27 13:32:38 +0600

    chore: modified some things

--- init.lua
+++ init.lua
@@ -38,9 +38,8 @@ require('lazy').setup({
 
   require 'twfksh.plugins.autopairs',
 
-  require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'twfksh.plugins.debugger',
+  -- require 'twfksh.plugins.lint',
 }, {
   performance = {
     rtp = {
